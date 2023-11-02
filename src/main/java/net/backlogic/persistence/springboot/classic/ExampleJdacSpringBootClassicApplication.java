7216d2746b6fc64958c2f93a49a2ab6c58032d0d
Ken Wang <kwbacklogic@gmail.com>
2023-11-01 19:55:39 -0700

    Minor changes

---
+++ src/main/java/net/backlogic/persistence/springboot/classic/ExampleJdacSpringBootClassicApplication.java
@@ -0,0 +1,11 @@
+package net.backlogic.persistence.springboot.classic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJdacSpringBootClassicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJdacSpringBootClassicApplication.class, args);
+	}
+}
